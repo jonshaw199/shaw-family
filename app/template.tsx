273f86b9d61c305bdfcb3487979492ed1365abc6
Jon Shaw <jonshaw199@gmail.com>
2024-08-27 15:44:57 -0700

    remove framer-motion dep; use react-bootstrap Fade

--- app/template.tsx
+++ app/template.tsx
@@ -1,25 +1,29 @@
 "use client";
 
-import { motion } from "framer-motion";
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Fade } from "react-bootstrap";
 import styles from "@/app/template.module.css";
+import { usePathname } from "next/navigation";
 
 export default function RootTemplate({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(false); // Start the fade out
+    const timeout = setTimeout(() => setShow(true), 300); // After fade out, trigger fade in
+    return () => clearTimeout(timeout);
+  }, [pathname]); // Trigger fade when the route changes
+
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 1 }} // Adjust the duration as needed
-    >
+    <Fade in={show} appear timeout={500}>
       <Container className={styles.content} fluid>
         {children}
       </Container>
-    </motion.div>
+    </Fade>
   );
 }
